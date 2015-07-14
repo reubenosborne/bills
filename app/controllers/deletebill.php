32e6e9a5a506bbe61ac43ccdcb510bb2d35cba65
Reuben Osborne <reuben.osborne@gmail.com>
2015-07-14 23:03:06 +1200

    Added delete

--- app/controllers/deletebill.php
+++ app/controllers/deletebill.php
@@ -8,11 +8,15 @@ $bill->load(Route::param('id'));
 
 $bill->delete();
 
-$accounts = new Accounts_Collection();
-$accounts->load(['bill_id' => $bill->id]);
+// $accounts = new Accounts_Collection();
+// $accounts->load(['bill_id' => $bill->id]);
 
-foreach ($variable as $key => $value) {
-	# code...
+$accounts = new Accounts_Collection();
+$accounts->where('bill_id' , $bill->id);
+$accounts->get();
+
+foreach ($accounts->items as $account) {
+	$account->delete();
 }
 
 # Redirect
